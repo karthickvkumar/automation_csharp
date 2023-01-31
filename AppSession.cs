1edb473ffbfc913a594bfe1a7c88db17f4efb2bc
kvelraj_concord <kvelraj@concord.net>
2023-01-31 17:31:21 +0530

    initial commit

---
+++ AppSession.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using WebDriverManager.DriverConfigs.Impl;
+using WebDriverManager;
+using OpenQA.Selenium.DevTools;
+using System.Threading;
+
+namespace Automation
+{
+    [TestClass]
+    public class AppSession
+    {
+        protected static IWebDriver webDriver;
+        public static void Setup(TestContext context)
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            webDriver = new ChromeDriver();
+
+            if (!object.ReferenceEquals(null, context.Properties["siteURL"]))
+            {
+                webDriver.Url = context.Properties["siteURL"].ToString();
+            }
+            else
+            {
+                webDriver.Url = "https://www.npmjs.com/";
+            }
+                
+            webDriver.Manage().Window.Maximize();
+        }
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            Setup(context);
+            webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            webDriver.Quit();
+        }
+
+        [TestMethod]
+        public void LoginToGmail_WithValidData_ShouldPass()
+        {
+            Thread.Sleep(4000);
+            var signinButton = webDriver.FindElement(By.Id("signin"));
+            signinButton.Click();
+
+            Thread.Sleep(2000);
+
+            var emailInput = webDriver.FindElement(By.Id("login_username"));
+            emailInput.Clear();
+            emailInput.SendKeys("karthick_fps");
+
+            var passwordInput = webDriver.FindElement(By.Id("login_password"));
+            passwordInput.Clear();
+            passwordInput.SendKeys("8056202457VKK@k");
+
+            var loginButton = webDriver.FindElement(By.XPath("//*[@id=\"login\"]/button"));
+            loginButton.Click();
+
+            Thread.Sleep(5000);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Automation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Automation")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("39da8aae-cdc7-4749-b4a0-232b37efc89b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
